0ca83451a6664686372b8032f794c812c2758f15
Oleg Anastassov <oleg.anastassov@gmail.com>
2022-02-27 23:40:15 +0300

    ## Convert to parameterized test

--- src/test/java/vms/api/BuildInfoControllerTest.java
+++ src/test/java/vms/api/BuildInfoControllerTest.java
@@ -2,14 +2,14 @@ package vms.api;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import java.time.Instant;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,48 +18,23 @@ class BuildInfoControllerTest {
 
   @Autowired BuildInfoController buildInfoController;
 
-  @Test
-  void shouldContainAllBuildInfoItems() {
+  @ParameterizedTest
+  @MethodSource("buildPropertiesProvider")
+  void buildPropertyValueContainValue(String key) {
     // when
-    var result = buildInfoController.getBuildInfo();
+    var build = buildInfoController.getBuildInfo();
+    var value = build.getProperty(key);
 
     // then
-    assertThat(
-        result.keySet(),
-        containsInAnyOrder("artifact", "group", "name", "time", "version"));
+    assertThat(value, is(notNullValue()));
   }
 
-  @Test
-  void shouldContainNonNullVersionInfo() {
-    // when
-    var result = buildInfoController.getBuildInfo();
-
-    // then
-    assertThat(result.getProperty("version"), is(notNullValue()));
-  }
-
-  @Test
-  void shouldContainNonNullTime() {
-    // when
-    var result = buildInfoController.getBuildInfo();
-    var timestamp = result.getProperty("time");
-
-    // then
-    assertThat(timestamp, is(notNullValue()));
-  }
-
-  @Disabled
-  @Test
-  void shouldContainTimeInUTC() {
-    // when
-    var result = buildInfoController.getBuildInfo();
-    var timestamp = result.getProperty("time");
-
-    // then
-    assertThat(timestamp, containsString("T"));
-    assertThat(timestamp, containsString("Z"));
-
-    var utc = Instant.parse(timestamp);
-    assertThat(utc, is(notNullValue()));
+  static Stream<Arguments> buildPropertiesProvider() {
+    return Stream.of(
+        arguments("artifact"),
+        arguments("group"),
+        arguments("name"),
+        arguments("time"),
+        arguments("version"));
   }
 }
